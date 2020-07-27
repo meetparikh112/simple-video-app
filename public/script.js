ba5d2169652b3e4e44cb730ca9b56473d4d6942f
Meet <meetparikh71@gmail.com>
2020-07-27 15:23:08 +0530

    secure app

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
 const myPeer = new Peer(undefined, {
 	host: '/',
-	secure: false,
+	secure: true,
 	port: '3001',
 });
 console.log('myPeer', myPeer);
